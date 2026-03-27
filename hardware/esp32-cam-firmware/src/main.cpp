eb92040fc273465b010a918e6546d72977f1ad78
Victor Hristov <victor.hristov@mentormate.com>
2026-03-27 16:52:34 +0200

    feat: enhance camera initialization with improved frame size and JPEG quality settings

--- hardware/esp32-cam-firmware/src/main.cpp
+++ hardware/esp32-cam-firmware/src/main.cpp
@@ -48,12 +48,19 @@ static bool initCamera() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_VGA;
-  config.jpeg_quality = 12;
+  config.frame_size = FRAMESIZE_XGA;
+  config.jpeg_quality = 8;
   config.fb_count = 1;
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
 
   esp_err_t err = esp_camera_init(&config);
+
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_brightness(s, 0);
+  s->set_contrast(s, 1);
+  s->set_saturation(s, 0);
+  s->set_whitebal(s, 1);
+  s->set_awb_gain(s, 1);
   return err == ESP_OK;
 }
 
